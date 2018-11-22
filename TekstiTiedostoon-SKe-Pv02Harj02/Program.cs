bbe8b6949313b7cafe2a8219fbaffefc83aae395
Sanna Ketomaki <sanna.ketomaki@stud.pointcollege.fi>
2018-11-22 14:47:56 +0200

    Add project files.

---
+++ TekstiTiedostoon-SKe-Pv02Harj02/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TekstiTiedostoon_SKe_Pv02Harj02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
