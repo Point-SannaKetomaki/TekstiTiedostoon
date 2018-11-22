27a24a52b0e1017970158fab33bc0f47f87613b9
Sanna Ketomaki <sanna.ketomaki@stud.pointcollege.fi>
2018-11-22 15:12:36 +0200

    Uusi koodi

--- TekstiTiedostoon-SKe-Pv02Harj02/Program.cs
+++ TekstiTiedostoon-SKe-Pv02Harj02/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,25 @@ namespace TekstiTiedostoon_SKe_Pv02Harj02
     {
         static void Main(string[] args)
         {
+            string polku = @"C:\Users\OWNER\Google Drive\PointCollege\Kurssi_05_Ohjelmoinnin jatkokurssi\Tekstiharjoitus.txt";
+            string nimi = "";
+
+            if (File.Exists(polku))
+            {
+                string aiempiNimi = File.ReadAllText(polku);
+                Console.WriteLine($"Aiemmin kirjoitettu nimi on {aiempiNimi}");
+            }
+            
+
+            Console.Write("Anna nimi: ");
+            nimi = Console.ReadLine();
+            File.WriteAllText(polku, nimi);
+            Console.WriteLine("Nimi on tallennettu muistiin.");
+
+            
+            
+            Console.ReadLine();
+
         }
     }
 }
